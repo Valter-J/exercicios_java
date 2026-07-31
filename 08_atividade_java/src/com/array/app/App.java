79739e0df987b3ee38a2ee315adc1b61b4d550a1
Valter-J <valterjpereiras@gmail.com>
2026-07-30 21:52:31 -0300

    teste com array

---
+++ 08_atividade_java/src/com/array/app/App.java
@@ -0,0 +1,26 @@
+package com.array.app;
+
+import com.array.models.ClasseTesteArrays;
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        ClasseTesteArrays array = new ClasseTesteArrays();
+
+
+        double [] testeArray = null;
+
+
+
+       array.inserirdeArrays(3, testeArray);
+
+        sc.close();
+
+
+    }
+}
---
+++ 08_atividade_java/src/com/array/models/ClasseTesteArrays.java
@@ -0,0 +1,44 @@
+package com.array.models;   
+
+import java.util.Scanner;
+
+
+public class ClasseTesteArrays {
+
+
+    private int numeroDeArrays;
+
+
+    public int getNumeroDeArrays() {
+        return this.numeroDeArrays;
+    }
+
+    public void setNumeroDeArrays(int numeroDeArrays) {
+        this.numeroDeArrays = numeroDeArrays;
+    }
+
+    public ClasseTesteArrays(){
+
+    }
+
+
+    public double[] inserirdeArrays(int numeroDeArrays, double[] nomeDoArray ){
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Digite o que é proposto dentro do array");
+        for ( int i =0; i < numeroDeArrays; i++){
+            
+            nomeDoArray[i]= sc.nextDouble();
+
+        }
+
+        sc.close();
+
+        return nomeDoArray;
+
+    }
+
+
+}
